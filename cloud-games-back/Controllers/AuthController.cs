2230cca8127d005bf583cc30d3bde3d67d7244e6
Duh269 <eduar@hotmail.com>
2025-09-15 21:11:45 -0300

    adicionado controller para autenticação

---
+++ cloud-games-back/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cloud_games_back.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
