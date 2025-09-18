6eccd58d476d6565670c143aa5872d73dafe2a71
Bruno Loures <bruno.loures@hotmail.com>
2025-09-17 21:38:06 -0300

    chore: rename default branch to main and move solution into src layout
    - TODO: alterar branch principal para 'main' e nome do projeto para incluir o front no futuro
    - projetos reorganizados dentro de src/ para facilitar separação em camadas
    - preparação para futura inclusão de Blazor Web

---
+++ src/cloud-games.api/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cloud_games_back.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
