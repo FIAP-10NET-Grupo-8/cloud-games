dae2345ba3588330e210aa402d1b41c9737d84a2
Bruno Loures <bruno.loures@hotmail.com>
2025-11-01 20:40:41 -0300

    feat(cart): implement CartService + DTOs + contract to satisfy specs (GREEN)

--- src/Fiap.CloudGames.Application/Carts/Dtos/CartResponseDto.cs
+++ src/Fiap.CloudGames.Application/Carts/Dtos/CartResponseDto.cs
@@ -1,5 +1,12 @@
 ﻿namespace Fiap.CloudGames.Application.Carts.Dtos;
 
+/// <summary>Carrinho do usuário.</summary>
+/// <param name="CartId">Identificador do carrinho.</param>
+/// <param name="UserId">Identificador do usuário.</param>
+/// <param name="UserEmail">Email do usuário.</param>
+/// <param name="Total">Total do carrinho.</param>
+/// <param name="UpdatedAt">Última atualização.</param>
+/// <param name="Items">Itens do carrinho.</param>
 public sealed record CartResponseDto(
     Guid CartId,
     Guid UserId,
--- src/Fiap.CloudGames.Application/Carts/Dtos/CartSummaryDto.cs
+++ src/Fiap.CloudGames.Application/Carts/Dtos/CartSummaryDto.cs
@@ -1,5 +1,11 @@
 ﻿namespace Fiap.CloudGames.Application.Carts.Dtos;
 
+/// <summary>Resumo de carrinho (admin).</summary>
+/// <param name="CartId">Identificador do carrinho.</param>
+/// <param name="UserEmail">Email do dono.</param>
+/// <param name="ItemsCount">Quantidade de itens.</param>
+/// <param name="Total">Total do carrinho.</param>
+/// <param name="UpdatedAt">Última atualização.</param>
 public sealed record CartSummaryDto(
     Guid CartId,
     string UserEmail,
