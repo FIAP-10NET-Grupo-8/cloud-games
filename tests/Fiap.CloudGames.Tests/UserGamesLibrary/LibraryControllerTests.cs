7336472e8a0f081579dc829ae088883aaf1668de
JoseEduardoSalles <jose.salles@bandtec.com.br>
2025-11-02 20:09:26 -0300

    Feature: Adicionando os testes unitários da Biblioteca

---
+++ tests/Fiap.CloudGames.Tests/UserGamesLibrary/LibraryControllerTests.cs
@@ -0,0 +1,114 @@
+﻿using Fiap.CloudGames.Api.Controllers;
+using Fiap.CloudGames.Application.UserGamesLibrary.Dtos;
+using Fiap.CloudGames.Application.UserGamesLibrary.Services;
+using Fiap.CloudGames.Domain.Games.Entities;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.CloudGames.Tests.UserGamesLibrary
+{
+    public class LibraryControllerTests
+    {
+        private readonly Mock<ILibraryService> _mockService;
+        private readonly LibraryController _controller;
+        private readonly Guid _testUserId;
+
+        public LibraryControllerTests()
+        {
+            _mockService = new Mock<ILibraryService>();
+            _testUserId = Guid.NewGuid();
+
+            var userClaims = new Claim[] { new Claim("userId", _testUserId.ToString()) };
+            var userPrincipal = new ClaimsPrincipal(new ClaimsIdentity(userClaims, "TestAuth"));
+            _controller = new LibraryController(_mockService.Object)
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = new DefaultHttpContext { User = userPrincipal }
+                }
+            };
+        }
+
+        [Fact]
+        public async Task GetMyLibrary_QuandoChamado_DeveRetornarOkComListaDeJogos()
+        {
+            var queryParams = new LibraryQueryDto();
+            var jogoDeTeste = Game.Create(
+                    title: "Test Game",
+                    description: "Descrição de teste",
+                    price: 19.99m,
+                    releaseDate: DateTime.UtcNow.AddYears(-1),
+                    developer: "Dev Teste",
+                    publisher: "Pub Teste",
+                    genre: "RPG",
+                    platforms: "PC"
+                );
+
+            var listaDeJogos = new List<Game> { jogoDeTeste };
+
+            _mockService.Setup(s => s.GetGamesFromLibraryAsync(_testUserId, queryParams))
+                .ReturnsAsync(listaDeJogos);
+
+            var resultado = await _controller.GetMyLibrary(queryParams);
+
+            var okResult = resultado.Should().BeOfType<OkObjectResult>().Subject;
+            okResult.Value.Should().Be(listaDeJogos);
+        }
+
+        [Fact]
+        public async Task PurchaseGame_QuandoSucesso_DeveRetornarOk()
+        {
+            var gameId = Guid.NewGuid();
+            _mockService.Setup(s => s.AddGameToLibraryAsync(_testUserId, gameId))
+                .ReturnsAsync(true);
+
+            var resultado = await _controller.PurchaseGame(gameId);
+
+            resultado.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public async Task PurchaseGame_QuandoJogoJaExiste_DeveRetornarConflict()
+        {
+            var gameId = Guid.NewGuid();
+            _mockService.Setup(s => s.AddGameToLibraryAsync(_testUserId, gameId))
+                .ReturnsAsync(false);
+
+            var resultado = await _controller.PurchaseGame(gameId);
+
+            resultado.Should().BeOfType<ConflictObjectResult>();
+        }
+
+        [Fact]
+        public async Task RefundGame_QuandoSucesso_DeveRetornarNoContent()
+        {
+            var gameId = Guid.NewGuid();
+            _mockService.Setup(s => s.RemoveGameFromLibraryAsync(_testUserId, gameId))
+                .ReturnsAsync(true);
+
+            var resultado = await _controller.RefundGame(gameId);
+
+            resultado.Should().BeOfType<NoContentResult>();
+        }
+
+        [Fact]
+        public async Task RefundGame_QuandoJogoNaoEncontrado_DeveRetornarNotFound()
+        {
+            var gameId = Guid.NewGuid();
+            _mockService.Setup(s => s.RemoveGameFromLibraryAsync(_testUserId, gameId))
+                .ReturnsAsync(false);
+
+            var resultado = await _controller.RefundGame(gameId);
+
+            resultado.Should().BeOfType<NotFoundObjectResult>();
+        }
+    }
+}
---
+++ tests/Fiap.CloudGames.Tests/UserGamesLibrary/LibraryServiceTests.cs
@@ -0,0 +1,111 @@
+﻿using Fiap.CloudGames.Application.UserGamesLibrary.Dtos;
+using Fiap.CloudGames.Application.UserGamesLibrary.Services;
+using Fiap.CloudGames.Domain.Games.Entities;
+using Fiap.CloudGames.Domain.UserGamesLibrary.Repositories;
+using FluentAssertions;
+using UserGamesLibraryEntity = Fiap.CloudGames.Domain.UserGamesLibrary.Entities.UserGameLibrary;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.CloudGames.Tests.UserGamesLibrary
+{
+    public class LibraryServiceTests
+    {
+        private readonly Mock<IUserGameLibraryRepository> _mockRepo;
+        private readonly ILibraryService _sut;
+        private readonly Guid _testUserId;
+        private readonly Guid _testGameId;
+
+        public LibraryServiceTests()
+        {
+            _mockRepo = new Mock<IUserGameLibraryRepository>();
+            _sut = new LibraryService(_mockRepo.Object);
+            _testUserId = Guid.NewGuid();
+            _testGameId = Guid.NewGuid();
+        }
+
+        [Fact]
+        public async Task AddGameToLibraryAsync_QuandoJogoNaoExiste_DeveAdicionarComSucesso()
+        {
+            _mockRepo.Setup(r => r.GetAsync(_testUserId, _testGameId))
+                .ReturnsAsync((UserGamesLibraryEntity)null);
+
+            var resultado = await _sut.AddGameToLibraryAsync(_testUserId, _testGameId);
+
+            resultado.Should().BeTrue();
+            _mockRepo.Verify(r => r.AddAsync(It.IsAny<UserGamesLibraryEntity>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task AddGameToLibraryAsync_QuandoJogoJaExiste_DeveRetornarFalso()
+        {
+            _mockRepo.Setup(r => r.GetAsync(_testUserId, _testGameId))
+                .ReturnsAsync(new UserGamesLibraryEntity());
+
+            var resultado = await _sut.AddGameToLibraryAsync(_testUserId, _testGameId);
+
+            resultado.Should().BeFalse();
+            _mockRepo.Verify(r => r.AddAsync(It.IsAny<UserGamesLibraryEntity>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task RemoveGameFromLibraryAsync_QuandoJogoExiste_DeveRemoverComSucesso()
+        {
+            var jogoNaBiblioteca = new UserGamesLibraryEntity { UserId = _testUserId, GameId = _testGameId };
+            _mockRepo.Setup(r => r.GetAsync(_testUserId, _testGameId))
+                .ReturnsAsync(jogoNaBiblioteca);
+
+            var resultado = await _sut.RemoveGameFromLibraryAsync(_testUserId, _testGameId);
+
+            resultado.Should().BeTrue();
+            _mockRepo.Verify(r => r.DeleteAsync(jogoNaBiblioteca), Times.Once);
+        }
+
+        [Fact]
+        public async Task RemoveGameFromLibraryAsync_QuandoJogoNaoExiste_DeveRetornarFalso()
+        {
+            _mockRepo.Setup(r => r.GetAsync(_testUserId, _testGameId))
+                .ReturnsAsync((UserGamesLibraryEntity)null);
+
+            var resultado = await _sut.RemoveGameFromLibraryAsync(_testUserId, _testGameId);
+
+            resultado.Should().BeFalse();
+            _mockRepo.Verify(r => r.DeleteAsync(It.IsAny<UserGamesLibraryEntity>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task GetGamesFromLibraryAsync_DeveChamarRepositorio_ComParametrosTraduzidos()
+        {
+            var queryParams = new LibraryQueryDto { Categoria = "RPG" };
+
+            var jogoDeTeste = Game.Create(
+                    title: "Test Game",
+                    description: "Descrição de teste",
+                    price: 19.99m,
+                    releaseDate: DateTime.UtcNow.AddYears(-1),
+                    developer: "Dev Teste",
+                    publisher: "Pub Teste",
+                    genre: "RPG",
+                    platforms: "PC"
+                );
+
+            var listaDeJogosEsperada = new List<Game> { jogoDeTeste };
+
+            _mockRepo.Setup(r => r.GetGamesByUserIdAsync(
+                    _testUserId,
+                    null, "RPG", null, null, null, null
+                ))
+                .ReturnsAsync(listaDeJogosEsperada);
+
+            var resultado = await _sut.GetGamesFromLibraryAsync(_testUserId, queryParams);
+
+            resultado.Should().HaveCount(1);
+            resultado.First().Title.Should().Be("Test Game");
+            _mockRepo.Verify();
+        }
+    }
+}
